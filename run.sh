d98de3ba21ca5a75387ad6e13cef9ed4a524c957
Erick <palmoreck@gmail.com>
2019-11-30 22:39:09 -0600

    adding lines for home variable

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+NB_USER=jovyan
+HOME=/home/${NB_USER}
 cd ${HOME} && git clone https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
 /usr/local/bin/jupyter lab --ip=0.0.0.0 --no-browser
