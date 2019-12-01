c810d9f6dbb459e644628a17254fe75fa1825413
Erick <palmoreck@gmail.com>
2019-11-30 22:50:30 -0600

    deleting line for jupyter lab and adding exec cmd

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 NB_USER=jovyan
 HOME=/home/${NB_USER}
 cd ${HOME} && git clone https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
-/usr/local/bin/jupyter lab --ip=0.0.0.0 --no-browser
+
+exec "$@"
