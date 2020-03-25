27709a07c24ef730bca4bad95b27c794303f8b88
erick <palmoreck@gmail.com>
2020-03-25 17:13:47 -0600

    modifying for test2 docker image

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ NB_USER=jovyan
 HOME=/home/${NB_USER}
 cd ${HOME} && git clone -b master --single-branch https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
 
-cd ${HOME} && bash ${HOME}/run.sh
+cd ${HOME} && bash ${HOME}/run_installations.sh
 
 exec "$@"
