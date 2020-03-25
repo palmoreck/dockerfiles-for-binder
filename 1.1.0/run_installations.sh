335e82ff8b77039c55164783407f886fa0060f00
erick <palmoreck@gmail.com>
2020-03-25 17:46:12 -0600

    adding files for binder

---
+++ 1.1.0/run_installations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER=jovyan
+HOME=/home/${USER}
+
+sudo apt-get update && sudo apt-get install -y libopenblas-dev
+sudo apt-get update && \
+     sudo apt-get install -y r-base libssl-dev libxml2-dev libcurl4-openssl-dev
+sudo chmod gou+w /usr/local/lib/R/site-library && \
+    sudo R -e 'install.packages(c("repr", "IRdisplay", "evaluate", "crayon", "pbdZMQ", "devtools", "uuid", "digest"), lib="/usr/local/lib/R/site-library/")' && \
+    R -e 'devtools::install_github("IRkernel/IRkernel")' && \
+    sudo R -e 'IRkernel::installspec(user=FALSE)'
+sudo chmod gou+wx -R /home/jovyan/.local
