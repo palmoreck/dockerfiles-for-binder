9d49c5f6ae025ebbb8fa232b486ccd775bc7f6c8
erick <palmoreck@gmail.com>
2020-03-25 16:30:14 -0600

    adding files for binder button openblas

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NB_USER=jovyan
+HOME=/home/${NB_USER}
+cd ${HOME} && git clone -b master --single-branch https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
+
+cd ${HOME} && bash ${HOME}/run.sh
+
+exec "$@"
