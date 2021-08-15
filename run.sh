873689a1763e8569e6aeeaa19171b6d31d45fd23
Erick Palacios Moreno <palmoreck@gmail.com>
2021-08-15 13:15:22 -0500

    changing directory and build

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 NB_USER=jovyan
 HOME=/home/${NB_USER}
 cd ${HOME} && git clone -b master --single-branch https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
-mv _toc.yml analisis-numerico-computo-cientifico/libro_optimizacion/temas/ && pip install networkx && jb build . --builder pdfhtml 
+mv _toc.yml analisis-numerico-computo-cientifico/libro_optimizacion/temas/ && cd analisis-numerico-computo-cientifico/libro_optimizacion/temas/ && jb build . --builder pdfhtml 
 exec "$@"
