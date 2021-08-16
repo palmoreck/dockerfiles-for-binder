059bbdb39fab4c35f2a32d97b96e4dd245f5e9a6
Erick Palacios Moreno <palmoreck@gmail.com>
2021-08-16 10:45:25 -0500

    using main branch instead of master

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b master --single-branch https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
+cd ${HOME} && git clone -b main --single-branch https://github.com/ITAM-DS/analisis-numerico-computo-cientifico.git
 exec "$@"
