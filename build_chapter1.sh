ad4b5844b07866473355314700557187f374f1b3
Erick Palacios Moreno <palmoreck@gmail.com>
2021-08-17 18:01:52 -0500

    Delete build_chapter1.sh

--- build_chapter1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd analisis-numerico-computo-cientifico/libro_optimizacion/temas/
-cp old_tocs/_toc1.yml _toc.yml
-jb build . --builder pdfhtml
-mv _build/pdf/book.pdf _build/pdf/chapter1_opt_book.pdf
