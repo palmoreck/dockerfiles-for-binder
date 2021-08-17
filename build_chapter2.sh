cd63fac4f6de90127f27aa16b4ba7c1e79e0aa9a
Erick Palacios Moreno <palmoreck@gmail.com>
2021-08-17 16:14:27 -0500

    using path of old_tocs

--- build_chapter2.sh
+++ build_chapter2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd analisis-numerico-computo-cientifico/libro_optimizacion/temas/
-mv _toc2.yml _toc.yml
+mv old_tocs/_toc2.yml _toc.yml
 jb build . --builder pdfhtml
 mv _build/pdf/book.pdf _build/pdf/chapter2_opt_book.pdf
