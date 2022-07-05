9c8c63aea4f7deca12cc2119fb61ae0b5b9a8f02
Erick Palacios Moreno <palmoreck@gmail.com>
2022-07-05 00:27:47 -0500

    returning to git clone

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} 
-
-#&& git clone -b main --single-branch https://github.com/ITAM-DS/Propedeutico.git
+cd ${HOME} && git clone -b main --single-branch https://github.com/ITAM-DS/Propedeutico.git
 
 exec "$@"
