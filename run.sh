40959f2d2e053f486a4c0009510afb30da71880a
Erick Palacios Moreno <palmoreck@gmail.com>
2022-07-04 23:49:26 -0500

    not cloning repo

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b main --single-branch https://github.com/ITAM-DS/Propedeutico.git
+cd ${HOME} 
+
+#&& git clone -b main --single-branch https://github.com/ITAM-DS/Propedeutico.git
 
 exec "$@"
